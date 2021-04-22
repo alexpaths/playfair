30e56aa6a618d0b1d738382338ca7ea49a0852ec
Alexander Athougies <blackmarlinholding@gmail.com>
2021-04-22 06:51:44 -0700

    finished script and MD file output

--- playfair.py
+++ playfair.py
@@ -16,25 +16,48 @@ choices = string.ascii_uppercase + string.digits
 grid = []
 gridsize = 6
 
-for j in range(0, gridsize):
+for j in range(0, gridsize + 1):
     gridrow = []
-    for i in range(0, gridsize):
-        choice = random.choice(choices)
-        while (any(choice in s for s in grid) or any(choice in s for s in gridrow)):
+    if j == 1:
+	for i in range(0, gridsize):
+            gridrow.append('---')
+    else:
+        for i in range(0, gridsize):
             choice = random.choice(choices)
-        gridrow.append(choice)
+            while (any(choice in s for s in grid) or any(choice in s for s in gridrow)):
+                choice = random.choice(choices)
+            gridrow.append(choice)
     grid.append(gridrow)
+
+## Modify to Markdown
+
+PFmatrix = ''
+for j in grid:
+    rowstring = '| '
+    for i in j:
+        rowstring += i + ' | '
+    rowstring += ' \n'
+    PFmatrix += rowstring
 
 ## Output to file
 
-print(grid)
+matrixCopies = 2
 
-fname = today.strftime("%Y%m%d") + '.md'
+fID = ''
+for i in grid[0]:
+    fID += i
+
+fname = 'output/' + today.strftime("%Y%m%d") + '_' + fID + '.md'
 f = open(fname, 'w')
 
-#for x in range(0,2):
-#    f.write = '# ' + today.strftime("%Y%m%d") + '  \n'
-#    f.write = '<br>'
+for x in range(0, matrixCopies):
+    f.write('# ' + today.strftime("%Y%m%d") + '\n')
+    f.write('\n')
+    f.write(PFmatrix)
+    f.write('\n<br>\n')
 
+f.write('For Academic Purposes Only - GPL-3.0-only  \n')
+f.write('[Source Code](https://github.com/alexpaths/playfair)\n')
+f.close()
 
-#Move matrix into md file twice... with date directly above and hr separator 
+## EOF
