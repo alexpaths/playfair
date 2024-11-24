5395f10d6349776322262139d8f583a43ba1f4a6
Alexander Athougies <alexpaths@gmail.com>
2024-11-23 23:57:25 -0800

    overcomplicating this using classes

--- playfair.py
+++ playfair.py
@@ -10,34 +10,60 @@ See README.md for details'''
 from datetime import date
 import string
 import random
+from icecream import ic
 
-## Global Variables
-CHOICES = ''.join([string.ascii_uppercase,string.digits])
-output = [*range(0,len(CHOICES),1)]
+class PlayfairMatrix():
+    def __init__(self):
+        self.choices = ''.join([string.ascii_uppercase,string.digits])
+        self.indexes = [*range(0,len(self.choices),1)]
+        self.CreateNewMatrix()
+    
+    def CreateNewMatrix(self):
+        random.shuffle(self.indexes)
+        self.output = [self.choices[i] for i in self.indexes]
 
-## Prepare Cipher Matrix
-random.shuffle(output)
+    def ConvertMarkdown(self):
+        self.markdown = [
+            ''.join(['|','|'.join(self.output[0:6]),'|\n']),
+            '|:---:|:---:|:---:|:---:|:---:|:---:|\n',
+            ''.join(['|','|'.join(self.output[6:12]),'|\n']),
+            ''.join(['|','|'.join(self.output[12:18]),'|\n']),
+            ''.join(['|','|'.join(self.output[18:24]),'|\n']),
+            ''.join(['|','|'.join(self.output[24:30]),'|\n']),
+            ''.join(['|','|'.join(self.output[30:37]),'|\n'])]
 
-i = 0
-for num in output:
-    output[i] = CHOICES[num]
-    i = i + 1
+    # TODO Fix this
+    # def addMarkdownFooter():
+        # \n---\n',
+        # 'For Academic Purposes Only - GPL-3.0-only  \n\n',
+        # '[Source Code](https://github.com/alexpaths/playfair)\n']
 
-# print (str(''.join(output)))
+    def ExportMarkdown(self):
+        self.ConvertMarkdown()
+        FNAME = ''.join(['output/',date.today().strftime("%Y-%m-%d"),'_',''.join(self.output[0:6]),'.md'])
+        with open(FNAME, mode='w', encoding='utf-8') as f:
+            f.write(''.join(['## ', date.today().strftime("%Y-%m-%d"),'\n\n']))
+            for line in self.markdown:
+                f.write(line)
 
-## Output to file
-FNAME = ''.join(['output/',date.today().strftime("%Y-%m-%d"),'_',''.join(output[0:6]),'.md'])
-with open(FNAME, mode='w', encoding='utf-8') as f:
-    f.write(''.join(['# ', date.today().strftime("%Y-%m-%d"),'\n\n']))
-    f.write(''.join(['|','|'.join(output[0:6]),'|\n']))
-    f.write('|:---:|:---:|:---:|:---:|:---:|:---:|\n')
-    f.write(''.join(['|','|'.join(output[6:12]),'|\n']))
-    f.write(''.join(['|','|'.join(output[12:18]),'|\n']))
-    f.write(''.join(['|','|'.join(output[18:24]),'|\n']))
-    f.write(''.join(['|','|'.join(output[24:30]),'|\n']))
-    f.write(''.join(['|','|'.join(output[30:37]),'|\n']))
-    f.write('\n---\n')
-    f.write('For Academic Purposes Only - GPL-3.0-only  \n\n')
-    f.write('[Source Code](https://github.com/alexpaths/playfair)\n')
+    def ExportMarkdownSheet(self,num):
+        FNAME = ''.join(['output/',date.today().strftime("%Y-%m-%d"),'_SHEET.md'])
+        with open(FNAME, mode='w', encoding='utf-8') as f:
+            f.write(''.join(['## ', date.today().strftime("%Y-%m-%d"),'\n\n']))
+            for click in range(num):
+                self.CreateNewMatrix()
+                self.ConvertMarkdown()
 
-## EOF
+                f.write(''.join(['# Code ', str(click), '\n']))
+                
+                for line in self.markdown:
+                    f.write(line)
+                
+                f.write('---\n')
+
+
+if __name__ == "__main__":
+    data = PlayfairMatrix()
+    data.ExportMarkdownSheet(5)
+
+# ## EOF
